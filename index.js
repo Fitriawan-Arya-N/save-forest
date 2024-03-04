4379fe3834ec494fc5b7e7d94adceb702ed351ab
Fitriawan Arya Nugraha <nugrahaaryaa76@gmail.com>
2024-03-04 13:20:37 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
+const totalImages = images.length;
+
+function showImage(index) {
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    showImage(currentIndex);
+}
+
+
+setInterval(nextImage, 10000);
+
+
+showImage(currentIndex);
